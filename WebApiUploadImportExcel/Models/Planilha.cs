940a379401205205ca285fc72ed815b1fb00d5f6
Marcio de Andrade <marcio@requestdev.net>
2024-05-30 10:53:09 -0400

    Configuração Middleware

---
+++ WebApiUploadImportExcel/Models/Planilha.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiUploadImportExcel.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Planilha> Planilhas { get; set; }
+    }
+
+
+    public class Planilha
+    {
+        public int Id { get; set; }
+        public string? Coluna1 { get; set; }
+        public string? Coluna2 { get; set; }
+    }
+}
